102633cf992c246dc5a7e576a199297935d7d91a
07nawraa <anindyanauraanindya@gmail.com>
2026-03-09 23:19:43 +0700

    Menambahkan perhitungan BMI dan kategori

--- dart_application_1/bin/main.dart
+++ dart_application_1/bin/main.dart
@@ -5,6 +5,21 @@ void main() {
   if (tinggi <= 0 || berat <= 0) {
     print("Input tidak valid");
   } else {
-    print("Input valid");
+
+    double tinggiMeter = tinggi / 100;
+    double bmi = berat / (tinggiMeter * tinggiMeter);
+
+    print("BMI kamu adalah: $bmi");
+
+    if (bmi < 18.5) {
+      print("Kategori: Kurus");
+    } else if (bmi < 25) {
+      print("Kategori: Normal");
+    } else if (bmi < 30) {
+      print("Kategori: Gemuk");
+    } else {
+      print("Kategori: Obesitas");
+    }
+    
   }
 }
